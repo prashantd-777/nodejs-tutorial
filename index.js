7bf0b5d8fd467881b83d4fdca9aeaecbd062fe5b
Prashant Dawkhar <dawkharp79@gmail.com>
2024-04-12 00:49:25 +0530

    Code changes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-const add = require("./add");
+// const add = require("./add");
 
-console.log("Hello from index.js");
-const sum = add(1, 2);
-console.log("sum is::", sum);
-const sum2 = add(4, 5);
-console.log(sum2);
+// console.log("Hello from index.js");
+// const sum = add(1, 2);
+// console.log("sum is::", sum);
+// const sum2 = add(4, 5);
+// console.log(sum2);
+
+
+let SuperHero = require("./superHero");
+let batman = new SuperHero("Batman");
+console.log(batman.getName());
+batman.setName("Bruce Wayne");
+console.log(batman.getName());
+
+let superman = new SuperHero("SuperMan");
+console.log(superman.getName());
+
+
