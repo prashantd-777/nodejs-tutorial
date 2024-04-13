86337a8702c3214cf126291ce5a145a77c7bca45
Prashant Dawkhar <dawkharp79@gmail.com>
2024-04-13 21:05:19 +0530

    Changes

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
+const math = require("./math");
 
-// const add = require("./add");
-
-// console.log("Hello from index.js");
-// const sum = add(1, 2);
-// console.log("sum is::", sum);
-// const sum2 = add(4, 5);
-// console.log(sum2);
-
-
-let SuperHero = require("./superHero");
-let batman = new SuperHero("Batman");
-console.log(batman.getName());
-batman.setName("Bruce Wayne");
-console.log(batman.getName());
-
-let superman = new SuperHero("SuperMan");
-console.log(superman.getName());
-
-
+console.log(math.add(2, 3));
+console.log(math.subtract(2, 3));
---
+++ main.mjs
@@ -0,0 +1,4 @@
+import math from "./math-esm.mjs";
+
+console.log(math.add(3, 5));
+console.log(math.subtract(3, 5));
