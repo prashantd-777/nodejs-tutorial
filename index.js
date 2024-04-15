45aeac8b287fce1d67e97d1ad958adceff32da89
Prashant Dawkhar <dawkharp79@gmail.com>
2024-04-15 17:09:20 +0530

    Built in events

---
+++ index.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+emitter.on("order-pizza", (size, topping) => {
+    console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+emitter.on('order-pizza', (size) => {
+    if(size === 'large') {
+        console.log("Serving complimentary drink");
+    }  
+});
+
+console.log("Do work before event occurs in the system");
+emitter.emit("order-pizza", "large", "mushroom");
