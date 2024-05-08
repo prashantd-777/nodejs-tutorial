311b9da5816dae79871e3fa268947639f5c16b8e
Prashant Dawkhar <dawkharp79@gmail.com>
2024-05-08 22:58:03 +0530

    Pipe

--- stream.js
+++ stream.js
@@ -2,11 +2,14 @@ const fs = require("node:fs");
 
 const readableStream = fs.createReadStream("./file.txt", {
     encoding: "utf-8",
-    // highWaterMark: 2
+    highWaterMark: 2
 });
 const writableStream = fs.createWriteStream("./file2.txt");
 
-readableStream.on("data", (chunk) => {
-    console.log("chunk data is::", chunk);
-    writableStream.write(chunk);
-})
+// readableStream.on("data", (chunk) => {
+//     console.log("chunk data is::", chunk);
+//     writableStream.write(chunk);
+// });
+
+readableStream.pipe(writableStream)
+
