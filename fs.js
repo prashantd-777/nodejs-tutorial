ee99dc7a1a39c0cbb83ebf082eeba2e83c1473d7
Prashant Dawkhar <dawkharp79@gmail.com>
2024-05-08 22:38:05 +0530

    Code changes

--- fs.js
+++ fs.js
@@ -1,15 +1,34 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
 console.log("first");
-const fileContents = fs.readFileSync("./file.txt", "utf-8");
 
-console.log("file contents", fileContents);
+fs.readFile("./file.txt", "utf-8").then(data => console.log("data is::", data)).catch(error => console.log("error is::", error));
+
 console.log("second");
 
-fs.readFile("./file.text", "utf-8", (error, data) => {
-    if(error) {
-        console.log("error is::", error)
-    } else {
-        console.log("data is::", data)
+async function readFile() {
+    try {
+        const data = await fs.readFile("./file.txt", "utf-8");
+        console.log("file data is::", data);
+    } catch(error) {
+        console.log("file error::", error);
     }
-})
+}
+
+readFile();
+
+// const fs = require("node:fs");
+
+// console.log("first");
+// const fileContents = fs.readFileSync("./file.txt", "utf-8");
+
+// console.log("file contents", fileContents);
+// console.log("second");
+
+// fs.readFile("./file.text", "utf-8", (error, data) => {
+//     if(error) {
+//         console.log("error is::", error)
+//     } else {
+//         console.log("data is::", data)
+//     }
+// })
